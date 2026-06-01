b01f8a308fd96e5f09ec3a5b0ab01880d4318d87
joellarramendia <joellarramendia@gmail.com>
2026-06-01 17:23:56 -0300

    feat: add Giphy API integration and implement GIF search functionality

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -4,6 +4,7 @@ import { PreviousSearches } from "./gifs/components/PreviousSearches"
 import { mockGifs } from "./mock-data/gifs.mock"
 import { CustomHeader } from "./shared/components/CustomHeader"
 import { SearchBar } from "./shared/components/SearchBar"
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action"
 
 export const GifsApp = () => {
     const [previousTerms, setPreviousTerms] = useState(['dragon ball z'])
@@ -12,7 +13,7 @@ export const GifsApp = () => {
         console.log({term})
     }
 
-    const handleSearch = (query: string = '') => {
+    const handleSearch = async(query: string = '') => {
         query = query.trim().toLowerCase()
 
         if(query.length === 0) return
@@ -20,6 +21,9 @@ export const GifsApp = () => {
         if(previousTerms.includes(query)) return
 
         setPreviousTerms([query, ...previousTerms].splice(0,8))
+
+        const gifs = await getGifsByQuery(query)
+        console.log({gifs})
     }
   return (
     <>
